5e6ef2014430ea1b30d1a7ae58d19db4aad9ab19
Hannah-Scheuer <hscheuer20@gmail.com>
2021-01-06 18:34:51 -0500

    created the add at end method

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,9 +1,17 @@
 public class MyLinkedList{
  private int size;
  private Node start,end;
- public MyLinkedList(){/*create a constructor*/}
+ public MyLinkedList(){
+   // I have no clue what to write for this constructor
+ }
  public int size();
- public boolean add(String value);
+ public boolean add(String value){
+   new Node temp = new Node(value);
+   temp.setPrev(end);
+   temp.setNext(numll);
+   end.setNext(temp);
+   return true;
+ }
  public void add(int index, String value);
  public String get(int index);
  public String set(int index, String value);
