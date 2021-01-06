7bf6ff6ea57aa60331c28b5d71bf9620417edf3c
Hannah-Scheuer <hscheuer20@gmail.com>
2021-01-06 18:57:27 -0500

    worked on size method

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -4,13 +4,16 @@ public class MyLinkedList{
  public MyLinkedList(){
    // I have no clue what to write for this constructor
  }
- public int size();
+ public int size(){
+   return size;
+ }
 
  public boolean add(String value){
    new Node temp = new Node(value);
    temp.setPrev(end);
    temp.setNext(numll);
    end.setNext(temp);
+   size += 1;
    return true;
  }
  public void add(int index, String value);
@@ -28,7 +31,7 @@ public class MyLinkedList{
  public String toString(){
    String out = "[";
    new Node temp = start;
-   for (int i = 0;i<size-1;i++){
+   for (int i = 0;i<size()-1;i++){
      out += temp.getData() + ", "
      temp = temp.next();
    }
