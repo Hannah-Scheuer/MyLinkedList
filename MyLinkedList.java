7776b235659bb9012ee39bf3ddc10dd61ae6d792
Hannah-Scheuer <hscheuer20@gmail.com>
2021-01-10 18:44:08 -0500

    slowly fixing error messages

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -9,38 +9,38 @@ public class MyLinkedList{
  }
 
  public boolean add(String value){
-   new Node temp = new Node(value);
+   Node temp = new Node(value);
    temp.setPrev(end);
-   temp.setNext(numll);
+   temp.setNext(null);
    end.setNext(temp);
    size += 1;
    return true;
  }
  public void add(int index, String value){
-   new Node temp = start;
+   Node temp = start;
    for (int i = 0;i<index-1;i++){
      temp = temp.next();
    }
-   new Node newNode = new Node(value);
-   newNode.setPrev(temp.getPrev);
-   newNode.setNex(temp.getNext);
+   Node newNode = new Node(value);
+   newNode.setPrev(temp.getPrev());
+   newNode.setNex(temp.getNext());
    newNode.getPrev().setNext(newNode);
    newNode.getNext().setPrev(newNode);
    size += 1;
  }
 
  public String get(int index){
-   new Node temp = start;
+   Node temp = start;
    for (int i = 0;i<index-1;i++){
-     temp = temp.next();
+     temp = temp.getNext();
    }
    return temp.getData();
  }
 
  public String set(int index, String value){
-   new Node temp = start;
+   Node temp = start;
    for (int i = 0;i<index-1;i++){
-     temp = temp.next();
+     temp = temp.getNext();
    }
    String out = temp.getData();
    temp.setData(value);
@@ -49,18 +49,18 @@ public class MyLinkedList{
 
  public String toString(){
    String out = "[";
-   new Node temp = start;
+   Node temp = start;
    for (int i = 0;i<size()-1;i++){
-     out += temp.getData() + ", "
-     temp = temp.next();
+     out += temp.getData() + ", ";
+     temp = temp.getNext();
    }
    return out + end.getData()+ "]";
  }
 
  public String remove(int index){
-   new Node temp = start;
+   Node temp = start;
    for (int i = 0;i<index-1;i++){
-     temp = temp.next();
+     temp = temp.getNext();
    }
    String out = temp.getData();
    temp.getPrev().setNext(temp.getNext());
@@ -70,6 +70,10 @@ public class MyLinkedList{
  }
 
 
- public void extend(MyLinkedList other);
+ public void extend(MyLinkedList other){
+   end.setNext(other.get(0));
+   other.get(0).setPrev(get(size-1));
+   size += other.size();
+ }
  //Any helper method that returns a Node object MUST BE PRIVATE!
 }
